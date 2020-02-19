8470ecd40e67512d4c78a6c2440e51640f7483ae
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-02-19 21:13:17 +0000

    more

--- scripts/app.js
+++ scripts/app.js
@@ -6,52 +6,97 @@ function main() {
   const width = 9
   const gridStatus = []
   let newGridStatus
+  let truthCounter = 0
 
   const testGrid = [
-    [9,1,0,0,0,8,0,2,0],
-    [0,8,7,3,4,0,9,0,5],
-    [2,5,0,0,9,7,0,0,0],
-    [0,7,0,0,0,5,0,3,0],
-    [1,0,2,0,3,4,0,0,8],
-    [0,0,8,2,0,9,7,0,4],
-    [0,0,0,0,5,0,3,0,0],
-    [0,0,0,0,0,3,2,0,6],
-    [3,4,0,0,2,6,8,0,1]
+    [9, 1, 0, 0, 0, 8, 0, 2, 0],
+    [0, 8, 7, 3, 4, 0, 9, 0, 5],
+    [2, 5, 0, 0, 9, 7, 0, 0, 0],
+    [0, 7, 0, 0, 0, 5, 0, 3, 0],
+    [1, 0, 2, 0, 3, 4, 0, 0, 8],
+    [0, 0, 8, 2, 0, 9, 7, 0, 4],
+    [0, 0, 0, 0, 5, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0, 3, 2, 0, 6],
+    [3, 4, 0, 0, 2, 6, 8, 0, 1]
   ]
 
-  function possible(y, x, n, grid) {
+  const fullGrid = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1]
+  ]
 
-    if (grid[y].includes(n)) {
-      return false
-    }
-    
+  function possible(y, x, n) {
+
     for (let i = 0; i < 9; i++) {
-      if (grid[i][x] === n) {
+      if (testGrid[y][i] === n) {
         return false
       }
     }
-    
+
+    for (let i = 0; i < 9; i++) {
+      if (testGrid[i][x] === n) {
+        return false
+      }
+    }
+
     const xSquare = Math.floor(x / 3) * 3
     const ySquare = Math.floor(y / 3) * 3
-    for (let i = 0; i < 3; i++) {
 
+    for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (grid[ySquare + i][xSquare + j] === n) {
+        if (testGrid[ySquare + i][xSquare + j] === n) {
           return false
-        } 
+        }
       }
     }
     return true
   }
 
+  function unfinishedGrid(grid) {
+
+    for (let i = 0; i < 9; i++) {
+      if (grid[i].includes(0)) {
+        return true
+      }
+    }
+    return false
+
+  }
+
+
   function solve(grid) {
+    // for (let y = 0; y < 9; y++) {
+    //   for (let x = 0; x < 9; x++) {
+    //     if (grid[y][x] === 0) {
+    //       for (let n = 1; n < 10; n++) {
+    //         if (possible(y, x, n, grid)) {
+    //           grid[y][x] = n
+    //           solve(grid)
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+    // return grid
+
     for (let y = 0; y < 9; y++) {
       for (let x = 0; x < 9; x++) {
         if (grid[y][x] === 0) {
           for (let n = 1; n < 10; n++) {
-            if (possible(y, x, n, grid)) {
+            if (possible(y, x, n)) {
               grid[y][x] = n
               solve(grid)
+            }
+            if (y === 1 && x === 7) {
+              console.log(possible(y, x, n))
+              grid[y][x] = 1
             }
           }
         }
@@ -91,7 +136,6 @@ function main() {
   submit.addEventListener('click', (e) => handleSubmit(e))
 
 }
-
 
 
 window.addEventListener('DOMContentLoaded', main)
